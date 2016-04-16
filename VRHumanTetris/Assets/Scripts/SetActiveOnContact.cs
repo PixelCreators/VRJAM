522b4e4cdfbabc6e34b4f665719c10f646cf7f92
Artur <artur1947@o2.pl>
2016-04-16 19:58:38 +0200

    Deleted unused folders, Added new player

---
+++ VRHumanTetris/Assets/Scripts/SetActiveOnContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization;
+using ObjectManager = System.Runtime.Serialization.ObjectManager;
+
+public class SetActiveOnContact : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "Background" || other.gameObject.tag == "Boundary")
+        {
+            return;
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Jestem w ifie");
+            Debug.Log(other.gameObject.name);
+            gameObject.SetActive(false);
+            OwnObjectManager.Instance.LeftWall.SetActive(true);
+            OwnObjectManager.Instance.RightWall.SetActive(true);
+        }
+    }
+}
---
+++ VRHumanTetris/Assets/Scripts/WallRight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        OwnObjectManager.Instance.RightWall = this.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
