1c6f5782b5377458fd2a2a628469ec1445e56f9d
Artur <artur1947@o2.pl>
2016-04-16 07:37:04 +0200

    Added GameController, restart, boundary

---
+++ VRHumanTetris/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private bool gameOver;
+    private bool restart;
+
+    void Start () {
+        restart = false;
+        gameOver = false;
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+    }
+}
