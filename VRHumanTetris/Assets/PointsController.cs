bfecafaa914d31b06fce2605f22ad5156d3f5b7d
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-18 00:22:17 +0200

    Added points scene and bonuses

---
+++ VRHumanTetris/Assets/PointsController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointsController : MonoBehaviour
+{
+    public static Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    text = GetComponent<Text>();
+	    text.text = "0 YouN00b";
+	}
+
+    public static void UpdateText(int Text)
+    {
+
+        text.text = Text.ToString();
+    }
+
+}
---
+++ VRHumanTetris/Assets/PointsManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointsManager : MonoBehaviour
+{
+
+    private static int Points;
+
+
+    public static void AddPoints()
+    {
+        Points += Random.Range(100, 300);
+        PointsController.UpdateText(Points);
+    }
+
+    public static void AddBonus()
+    {
+        Points += 1000;
+        PointsController.UpdateText(Points);
+    }
+}
