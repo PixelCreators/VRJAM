6cd59b67df67578466788148fd948aeaf86e4a45
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-17 22:33:56 +0200

    Added first game mechanic

--- VRHumanTetris/Assets/Hold.cs
+++ VRHumanTetris/Assets/Hold.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Hold : MonoBehaviour
 {
     public Transform cameraPosition;
+    
 
     void LateUpdate()
     {
+        cameraPosition = PlayerController.Instance.transform;
          transform.rotation = Quaternion.Euler(new Vector3((int)cameraPosition.eulerAngles.x, (int)cameraPosition.eulerAngles.y, (int)cameraPosition.eulerAngles.z - 90));
     }
 }
