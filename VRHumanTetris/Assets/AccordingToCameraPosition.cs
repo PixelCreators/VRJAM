bca4f39185f782572a3f0fbafd5c50c405603be8
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-17 03:20:35 +0200

    Skiing

---
+++ VRHumanTetris/Assets/AccordingToCameraPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccordingToCameraPosition : MonoBehaviour
+{
+    public Transform cameraPosition;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(cameraPosition.position.x + offset.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ VRHumanTetris/Assets/Hold.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hold : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    void FixedUpdate()
+    {
+         transform.rotation = Quaternion.Euler(new Vector3((int)cameraPosition.eulerAngles.x, (int)cameraPosition.eulerAngles.y, (int)cameraPosition.eulerAngles.z - 90));
+    }
+}
