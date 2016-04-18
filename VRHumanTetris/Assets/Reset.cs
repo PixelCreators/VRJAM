5d0543d458c27ba9884f3f14ee7c2004eda236a8
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-18 03:03:50 +0200

    Added things

---
+++ VRHumanTetris/Assets/Reset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using GamepadInput;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
+            SceneManager.LoadScene("Level");
+	}
+}
