38c1533ef63466abe2766fe6c24927860698b280
Artur <artur1947@o2.pl>
2016-04-18 04:18:41 +0200

    last

--- VRHumanTetris/Assets/Scripts/DestroyByPosition.cs
+++ VRHumanTetris/Assets/Scripts/DestroyByPosition.cs
@@ -9,9 +9,10 @@ public class DestroyByPosition : MonoBehaviour
 
     public void Update()
     {
-        if ((gameObject.transform.position.x > 40) || (gameObject.transform.position.x < -30) ||
-            (gameObject.transform.position.y > 20) || (gameObject.transform.position.y < -20) ||
-            (gameObject.transform.position.z > 50.5) || (gameObject.transform.position.z < -20.5))
+        Debug.Log(gameObject.transform.position);
+        if ((gameObject.transform.position.x > 6) || (gameObject.transform.position.x < -6) ||
+            (gameObject.transform.position.y > 10) || (gameObject.transform.position.y < -5) ||
+            (gameObject.transform.position.z > 25) || (gameObject.transform.position.z < -4))
         {
             Destroy(gameObject);
         }
